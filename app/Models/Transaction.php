6507e7ce82989ec6f160aec4bbb88295fa2b39c0
BlastTea <arya.jbr.999@gmail.com>
2024-07-19 00:54:46 +0700

    feat: has many at transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends BaseModel
 {
@@ -18,5 +19,10 @@ class Transaction extends BaseModel
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function transactionDetails(): HasMany
+    {
+        return $this->hasMany(TransactionDetail::class);
     }
 }
